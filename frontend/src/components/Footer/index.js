dcbf119510f8100125f3fe9901c9fd6b5a8a675e
EHDdS <erickhenriquedds@gmail.com>
2021-10-19 15:44:08 -0300

    FEAT: footer

--- frontend/src/components/Footer/index.js
+++ frontend/src/components/Footer/index.js
@@ -1,19 +1,31 @@
 import React from 'react';
-//import './style.css';/../assets
-import {ReactComponent as CommitJr} from'../../assets/commitjr.svg';
-import {ReactComponent as GitHubIcon} from '../../assets/github.svg'
+import './style.css';
+import {ReactComponent as CommitJr} from'../../assets/commitjr_new_cinza.svg';
+import {ReactComponent as GitHubIcon} from '../../assets/github.svg';
+import {ReactComponent as LogoIcon} from '../../assets/erick_logo.svg';
+
+function date() {
+    return new Date().getFullYear();
+}
 
 function Footer() {
     return (
-    <footer className="main-footer">
-
-            Made by Erick Henrique<br></br>
-            <div className="footer-icons">
+        <footer className="main-footer">
+            <div className="footer-icon-developer">
+                Developed by <a href="https://www.linkedin.com/in/erickhenriquedds/">Erick Henrique</a><br></br><br></br>
                 <a href="https://github.com/ErickHDdS" target="_new">
-                    <GitHubIcon />
+                    <GitHubIcon /><br></br><br></br>   
                 </a>
+                Copyright © {date()}
+            </div>
+            <div className="footer-icon-commit">
                 <a href="https://www.linkedin.com/company/commitjr/?originalSubdomain=br" target="_new">
                     <CommitJr />
+                </a>
+            </div>
+            <div className="footer-icon-logo">
+                <a href="http://localhost:3000/" target="_new">
+                    <LogoIcon />
                 </a>
             </div>
         </footer>
