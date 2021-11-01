384d09f89066aee992ab9186ac4bb96f658ae9fe
EHDdS <erickhenriquedds@gmail.com>
2021-10-31 21:56:54 -0300

    FIX Fixing 'cors' conflicts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,14 +1,17 @@
 import express from 'express';
-//import cors from 'cors';
+import cors from "cors";
 import router from './API/routes';
-
 const app = express();
-const port = 1403;
 
-//app.use(cors());
 app.use(express.json());
-app.use(router);
 
-app.listen(port, () => {
-    console.log(`Rodando a aplicação em: http://localhost:${port}`);
-});
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", 'GET, PUT, POST');
+    app.use(cors());
+    next();
+})
+
+app.use(router)
+
+app.listen(1403);
