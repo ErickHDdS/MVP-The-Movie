657f4342f12c2ed0cae026250fef086f46a471c7
EHDdS <erickhenriquedds@gmail.com>
2021-11-02 16:23:38 -0300

    FEAT: add new routes on frontend

--- frontend/src/APIs/GetGenre.js
+++ frontend/src/APIs/GetGenre.js
@@ -1,25 +1,20 @@
 import api from './baseAPI';
 
 async function getGenre() {
-    try {
-        const response = await api.get("/genres");      
-            if(response.status === 200){
-                const data = response.data;
-                return ({
-                    data: data,
-                    status: true,
-                });
-            };
+    const response = await api.get("/genres");
+    if (response.status === 200) {
+        const data = response.data;
+        return ({
+            data: data,
+            status: true,
+        });
+    };
 
-            return ({
-                data: response.data,
-                status: false,
-            });
+    return ({
+        data: response.data,
+        status: false,
+    });
 
-    } catch(err){
-        throw err;
-        console.log("derro");
-    }
 };
 
 export default getGenre;
--- frontend/src/APIs/GetMovie.js
+++ frontend/src/APIs/GetMovie.js
@@ -1,66 +1,83 @@
 import api from "./baseAPI";
 
 async function getMovieListOfGenre(genre) {
-    try {
-        const response = await api.get(`/movie/list/${genre}`);
-            if(response.status === 200){
-                const movies = response.data;
-                return ({
-                    data: movies,
-                    status: true,
-                });
-            };
+    const response = await api.get(`/movie/list/${genre}`);
+    if (response.status === 200) {
+        const movies = response.data;
+        return ({
+            data: movies,
+            status: true,
+        });
+    };
 
-            return ({
-                data: response.data,
-                status: false
-            });
-
-    } catch(err){
-        throw err;
-    }
+    return ({
+        data: response.data,
+        status: false
+    });
 };
 
 async function getMovieDetail(movie) {
-    try {
-        const response = await api.get(`/movie/${movie}`);
-            if(response.status === 200){
-                const movies = response.data;
-                return ({
-                    data: movies,
-                    status: true,
-                });
-            };
+    const response = await api.get(`/movie/${movie}`);
+    if (response.status === 200) {
+        const movies = response.data;
+        return ({
+            data: movies,
+            status: true,
+        });
+    };
 
-            return ({
-                data: response.data,
-                status: false
-            });
-
-    } catch(err){
-        throw err;
-    }
+    return ({
+        data: response.data,
+        status: false
+    });
 };
 
 async function getMovieSimiliar(movie) {
-    try {
-        const response = await api.get(`/movie/similiar/${movie}`);
-            if(response.status === 200){
-                const movies = response.data;
-                return ({
-                    data: movies,
-                    status: true,
-                });
-            };
+    const response = await api.get(`/movie/similiar/${movie}`);
+    if (response.status === 200) {
+        const movies = response.data;
+        return ({
+            data: movies,
+            status: true,
+        });
+    };
 
-            return ({
-                data: response.data,
-                status: false
-            });
-
-    } catch(err){
-        throw err;
-    }
+    return ({
+        data: response.data,
+        status: false
+    });
 };
 
-export {getMovieListOfGenre, getMovieDetail, getMovieSimiliar};
+async function getMoviesRealiseList() {
+    const response = await api.get("/movie/realise");
+    if (response.status === 200) {
+        const data = response.data;
+        return ({
+            data: data,
+            status: true,
+        });
+    };
+
+    return ({
+        data: response.data,
+        status: false,
+    });
+};
+
+async function getMovieVideo(movie) {
+    const response = await api.get(`/video/${movie}`);
+    if (response.status === 200) {
+        const movies = response.data;
+        return ({
+            data: movies,
+            status: true,
+        });
+    };
+
+    return ({
+        data: response.data,
+        status: false
+    });
+};
+
+export { getMovieListOfGenre, getMovieDetail, getMovieSimiliar, getMoviesRealiseList, getMovieVideo };
